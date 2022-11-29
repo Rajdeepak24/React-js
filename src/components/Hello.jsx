f476b5973d12dc9b765e7396376e83a2a6132e20
Rajdeepka Vishwakarma <60125824+Rajdeepak24@users.noreply.github.com>
2022-11-30 02:32:04 +0530

    ReactFirstGit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hello.css'
+
+function Hello() {
+  return (
+     <>
+      <div>
+        <p>hello</p>
+      </div>
+     </>
+  )
+}
+
+export default Hello
